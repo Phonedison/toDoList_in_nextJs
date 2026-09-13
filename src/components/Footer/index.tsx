8fdf030fb27026dacda4a8a271293183f5611e35
Lucas Leal da Silva <lucasleal00100@gmail.com>
2026-09-13 19:43:51 -0300

    feat: atualiza componente Footer para exibir contagem dinâmica de tarefas e progresso

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,26 +1,33 @@
+import { useTask } from "@/contexts";
 import { ListCheck, Sigma } from "lucide-react";
 import { Alert } from "../Alert";
 import { CardContent } from "../ui/card";
 
-export const Footer = ({ value }: { value: number }) => {
+export const Footer = () => {
+  const { qtdTask } = useTask();
+  const { qtdItem, qtdItemConcluded } = qtdTask();
+  const progressPercentage =
+    qtdItem > 0 ? (qtdItemConcluded / qtdItem) * 100 : 0;
   return (
     <CardContent className="gap-4">
       <div className="flex gap-2 justify-between mt-4">
         <div className="flex gap-2 items-center">
           <ListCheck size={16} />
-          <p>Tarefas Concluidas (3/{value})</p>
+          <p>
+            Tarefas Concluidas ({qtdItemConcluded}/{qtdItem})
+          </p>
         </div>
         <Alert />
       </div>
       <div className="h-2 w-full bg-gray-100 mt-4 rounded-md">
         <div
-          className="h-full bg-emerald-700 rounded-md"
-          style={{ width: "50%" }}
+          className="h-full bg-emerald-700 rounded-md transition-all duration-300"
+          style={{ width: `${progressPercentage}%` }}
         ></div>
       </div>
       <div className="flex justify-end items-center mt-2 gap-2">
         <Sigma size={18} />
-        <p className="text-xs">{value} tarefas no total</p>
+        <p className="text-xs">{qtdItem} tarefas no total</p>
       </div>
     </CardContent>
   );
