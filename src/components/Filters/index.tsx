c121dd17cf84339f84b3b74b62950bff809abaa7
Lucas Leal da Silva <lucasleal00100@gmail.com>
2026-09-14 13:34:52 -0300

    feat: refatora componente Filter para utilizar estado e mapeamento dinâmico de badges

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,19 +1,41 @@
-import { Badge } from "@/components/ui/badge";
+import { useTask } from "@/contexts";
 import { ArrowBigRight, Check, List } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Badge } from "../ui/badge";
 import { CardContent } from "../ui/card";
 
 export const Filter = () => {
+  const { setFilterId } = useTask();
+  const listBadge = [
+    { id: 1, icon: <List />, text: "Todas" },
+    { id: 2, icon: <ArrowBigRight />, text: "Não finalizados" },
+    { id: 3, icon: <Check />, text: "Concluidas" },
+  ];
+  const [idSelect, setIdSelect] = useState(1);
+
+  useEffect(() => {
+    setFilterId(idSelect);
+  }, [setFilterId, idSelect]);
+
   return (
     <CardContent className="flex gap-2 mt-2">
-      <Badge className="cursor-pointer">
-        <List /> Todas
-      </Badge>
-      <Badge className="cursor-pointer" variant={"outline"}>
-        <ArrowBigRight /> Não finalizados
-      </Badge>
-      <Badge className="cursor-pointer" variant={"outline"}>
-        <Check /> Concluidas
-      </Badge>
+      {listBadge.map((item) => {
+        const isSelect = item.id === idSelect;
+        const handleFilter = () => {
+          setIdSelect(item.id);
+        };
+
+        return (
+          <Badge
+            key={item.id}
+            className="cursor-pointer"
+            variant={isSelect ? "default" : "outline"}
+            onClick={handleFilter}
+          >
+            {item.icon} {item.text}
+          </Badge>
+        );
+      })}
     </CardContent>
   );
 };
